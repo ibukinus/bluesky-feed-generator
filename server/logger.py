5886ea89df038b08b19702a0b14d2eb730dbc29e
ibukinus <10069593+ibukinus@users.noreply.github.com>
2024-09-01 06:00:17 +0000

    ログフォーマットの設定

--- server/logger.py
+++ server/logger.py
@@ -1,4 +1,4 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(process)s] [%(levelname)s] %(message)s")
